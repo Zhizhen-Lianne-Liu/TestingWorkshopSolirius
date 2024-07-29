5d2046dc769dfd9dbaa6e9ced34c5ad1e15f7bdc
Lianne <zhizhen.lianne.liu@gmail.com>
2024-07-29 12:55:49 +0100

    senario 2 - lianne

--- features/step_definitions/calculate_holiday_entitlement_steps.rb
+++ features/step_definitions/calculate_holiday_entitlement_steps.rb
@@ -11,11 +11,13 @@ And('I should see the homepage') do
 end
 
 When ("I click on the 'Start now' button") do
-    
+    click_link('Start now')
+    expect(page).to have_current_path('https://www.gov.uk/calculate-your-holiday-entitlement/y')
 end
 
 And("I select the option no for working irregular hours") do
-      
+
+    
 end
 
 And ('I select the option hours worked per week') do
@@ -23,7 +25,7 @@ And ('I select the option hours worked per week') do
 end
 
 And ('I select the option for a full leave year') do
-      
+
 end
 
 And ('I input 37.5 hours worked per week') do
@@ -40,4 +42,58 @@ end
 
 And ('I should see the total entitlement hours') do
     
+end
+
+
+
+
+And("I select the option yes for working irregular hours") do
+    choose('Yes', allow_label_click: true)
+    click_button('Continue')
+    expect(page).to have_current_path('https://www.gov.uk/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year')
+    
+end
+
+And ('I enter when the leave year starts') do
+    fill_in('Day', with: '1')
+    fill_in('Month', with: '1')
+    fill_in('Year', with: '2023')
+    click_button('Continue')
+    
+end
+
+And ('I select the option days worked per week') do
+    choose('days worked per week', allow_label_click: true)
+    click_button('Continue')
+    
+end
+
+And ('I select for someone leaving part way through a leave year') do
+    choose('for someone leaving part way through a leave year', allow_label_click: true)
+    click_button('Continue')
+end
+
+And ('I enter employment end date') do
+    fill_in('Day', with: '8')
+    fill_in('Month', with: '8')
+    fill_in('Year', with: '2023')
+    click_button('Continue')
+end
+
+And ('I enter 3 days worked per week') do
+    fill_in('Number of days worked per week?', with: '3')
+    click_button('Continue')
+end
+
+Then ('I should see the total entitlement hours irregular') do
+    expect(page).to have_content('The statutory holiday entitlement is 10.2 days holiday.')
+end
+
+And ('I should see the correct submitted answers irregular') do
+    expect(page).to have_css('dd.govuk-summary-list__value', text: 'Yes')
+    expect(page).to have_css('dd.govuk-summary-list__value', text: '1 January 2023')
+    expect(page).to have_css('dd.govuk-summary-list__value', text: 'days worked per week')
+    expect(page).to have_css('dd.govuk-summary-list__value', text: 'for someone leaving part way through a leave year')
+    expect(page).to have_css('dd.govuk-summary-list__value', text: '8 August 2023')
+    expect(page).to have_css('dd.govuk-summary-list__value', text: '3.0')
 end
